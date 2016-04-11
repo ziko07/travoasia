a260d1e8b76ceb550ba9e6fb54a515a03fa51797
nazrulcse <nazrulku07@gmail.com>
2016-04-11 22:45:08 +0600

    Landing page image

--- app/models/landing_page_image.rb
+++ app/models/landing_page_image.rb
@@ -16,13 +16,30 @@
 
 class LandingPageImage < ActiveRecord::Base
 
-  has_attached_file :image
+  has_attached_file :image, :styles => {
+                              :left => "620x300#",
+                              :right => "300x300#"
+                          }
 
   # process_in_background :image
-
+  before_save :set_dimensions!
   #validates_attachment_presence :image
   validates_attachment_size :image, :less_than => 9.megabytes
   validates_attachment_content_type :image,
                                     :content_type => ["image/jpeg", "image/png", "image/gif",
                                                       "image/pjpeg", "image/x-png"] #the two last types are sent by IE
+
+  def set_dimensions!
+    # Silently return, if there's no `width` and `height`
+    # Prevents old migrations from crashing
+    return unless self.respond_to?(:width) && self.respond_to?(:height)
+
+    geometry = extract_dimensions
+
+    if geometry
+      self.width = geometry.width.to_i
+      self.height = geometry.height.to_i
+    end
+  end
+
 end
